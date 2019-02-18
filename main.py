7820c6f73e6a96f76b4707aa1f5909de70473c4b
Oh Hye-Jun <katinor@4ears.net>
2019-02-18 17:05:53 +0900

    log system added

---
+++ main.py
@@ -0,0 +1,35 @@
+import re, requests, json, random, time, datetime, os, glob, asyncio, discord
+import quadra_config
+from quadra_config import db_connect 
+import threading
+from quadra_log_module import log_append
+
+DATABASE = db_connect()
+back_time = 6
+
+# def resync_db(sql):
+#    curs = sql.cursor()
+#    curs.execute("SELECT 1 LIMIT 1")
+#    threading.Timer(60 * 60 * back_time, resync_db(DATABASE)).start()
+
+# resync_db(DATABASE)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    log_append(text = "Bot is online now")
+
+@client.event
+async def on_message(message):
+    log_append(
+        user = message.author,
+        guild= message.guild,
+        category= "Listener",
+        text = message.content)
+    if message.author == client.user:
+        return
+    if message.content.startswith('4!hello'):
+        await message.channel.send('Hello!')
+
+client.run(quadra_config.BOT_TOKEN)
---
+++ quadra_log_module.py
@@ -0,0 +1,57 @@
+import time, os, glob
+
+
+LOG_PATH = "log/quadra_bot_log"
+if not os.path.exists('log'):
+    os.makedirs(os.path.join('log'))
+
+fp = open(LOG_PATH+".txt", 'a')
+fp.close()
+
+def log_append(user = None, guild = None, category = None, text = None, level = 0):
+    _now = time.localtime()
+
+    if level == 1: temp_level = "[WARN]"
+    elif level == 2: temp_level = "[EROR]"
+    else: temp_level = "[INFO]"
+
+    target = temp_level + "[%04d-%02d-%02d %02d:%02d:%02d]"% (_now.tm_year, _now.tm_mon, _now.tm_mday, _now.tm_hour, _now.tm_min, _now.tm_sec)
+    if user:
+        target += "triggered by : "+user.name + "#" + user.discriminator + " (id:"+str(user.id)+")"
+    else:
+        target += "triggered by System"
+
+    print("#0 - " + target)
+    text_target = target + " / "
+
+    if guild:
+        target = "from : " + guild.name + " (id:"+str(guild.id)+")"
+        print("#1 - " + target)
+        text_target += target + " / "
+    else:
+        print("#1 - from None")
+
+
+    target = ""
+    if category:
+        target += "category = " + category
+    target += " : "
+    if text:
+        target += text
+
+    print("#2 - " + target)
+    text_target+= target
+
+    if os.path.getsize(LOG_PATH+".txt") >= 102400:
+        filelist = glob.glob("log/*.*")
+        filenum = len(filelist)
+        filename = LOG_PATH+"_"+str(filenum)+".txt"
+        os.rename(LOG_PATH+".txt",filename)
+    fp = open(LOG_PATH+".txt", 'a')
+    fp.write(text_target+"\n")
+    fp.close()
+
+    return _now
+
+
+    
